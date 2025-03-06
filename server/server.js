e0430b058914851b05acf35330c9e5ef43435e6a
gokul911 <pngokul08764@gmail.com>
2025-03-06 10:13:43 +0530

    Rechecked authentication

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const sequelize = require("./database"); // Import Sequelize connection
 const petRouter = require('./routes/allpets');
+const authRouter = require('./routes/auth-routes');
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 5000;
 // app.use(cookieparser());
 app.use(express.json());
 
+app.use("/api/auth" , authRouter);
 app.use("/api/pets" , petRouter);
 
 app.listen(PORT,()=>{
